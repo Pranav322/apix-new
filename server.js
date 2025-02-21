693f4d916c3de93bbf5eb9461b0c6087c9c2265a
pranav322 <pranav88096@gmail.com>
2025-02-21 14:01:13 +0530

    upload _ .gitignore

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { exec } = require("child_process");
 const stream = require('stream');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
