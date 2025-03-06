7b21e58d924dfd7ca19bc49be5f008e54ea5b526
pranav322 <pranav88096@gmail.com>
2025-03-06 15:52:24 +0530

    tried fixing rental , not sure

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Movie = require('../models/Movie');
 const Rental = require('../models/Rental');
 const authenticateJWT = require('../middleware/auth');
